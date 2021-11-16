730f8970e62e2090bd0ee84ce9f3b48367fadd9e
carpcode <paulus.carsten@googlemail.com>
2021-11-16 22:08:01 +0100

    correct update step

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -157,14 +157,14 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // TODO: Radar updates
-    ekf_.H_ = H_laser_;
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.R_ = R_radar_;
+    ekf_.H_ =  Hj_;
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
 
   } else {
     // TODO: Laser updates
-    Hj_ = tools.CalculateJacobian(ekf_.x_);
-    ekf_.R_ = R_radar_;
-    ekf_.H_ =  Hj_; 
+    ekf_.H_ = H_laser_; 
     ekf_.Update(measurement_pack.raw_measurements_);
 
   }
