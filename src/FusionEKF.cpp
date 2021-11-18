6155c03762abc9a68d535f1d5f41f5bb5492d476
carpcode <paulus.carsten@googlemail.coom>
2021-11-18 14:24:58 +0100

    Fix missing R_laser

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -192,6 +192,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   } else {
     // TODO: Laser updates
+    ekf_.R_ = R_laser_;
     ekf_.H_ = H_laser_; 
     ekf_.Update(measurement_pack.raw_measurements_);
 
